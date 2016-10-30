dcc9be7b78753c2da4c933061977ea8ce521e0ec
nbabu001@ucr.edu <nbabu001@ucr.edu>
2016-10-30 01:35:22 +0000

    created main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
 #include <iostream>
 
 #include "CommandStack.h"
+#include "Terminal.h"
+#include "Or.h"
+#include "And.h"
+#include "Semi.h"
+
 
 int main(int argc, char** argv) {
     
+    while(1) {
+        
+        
+        
+        //print $user@host()
+        string str;
+        getline(cin, str);
+        if (!parse(str)) {
+            // if exit is called 
+            break;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     
+        
+        
+    }
     
     
     
     return 0;
+}
+
+
+
+
+
+bool parse() {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
