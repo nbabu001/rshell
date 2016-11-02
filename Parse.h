d31cbae4b8ede9815d33e89f8f4f403b1c388e01
nbabu001@ucr.edu <nbabu001@ucr.edu>
2016-11-02 12:06:25 +0000

    updated parse.h

--- Parse.h
+++ Parse.h
@@ -1,104 +1,156 @@
 #ifndef __PARSE__H__
 #define __PARSE__H__
 
-#include <iostream>
 #include <vector>
 #include <string>
-
+#include "Connector.h"
+#include "Inputstorage.h"
+#include "executecmd.h"
 #include <boost/tokenizer.hpp>
+#include <boost/range/iterator_range.hpp>
+
+// string convert(tokenizer::iterator &it, vector<string> &conversion) {
+//     conversion.at(0) = it*;
+//     string result = conversion.at(0);
+//     return result;
+// }
 
 bool parse(string input) {
     
+    cout << "1: inside parse: " << input << endl;
     //String Tokenizer
-    typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+    typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
     
     //Pass input into the tokenizer
-    tokenizer toke{input};
+    tokenizer toke(input);
     
-    tokenizer::iterator it;
-    it = toke.begin();
+    //Initialize iterator to the begining of toke
+    tokenizer::iterator it = toke.begin();
     
-    bool is_command_only = false;
-    String command;
+    //Data fields used for looping
+   
+    string command = "DID NOT UPDATE COMMAND";
+    string tester;
+    string compare;
     vector<string> args;
+    args.push_back("");
+    vector<string> placeholder(1);
     Connector* temp = 0;
+    string type = "";
     
     while(it != toke.end()) {
+        bool is_command_only = false;
+        bool skipper = false;
         
+        cout << "one loop" << endl;
         //START OF THE COMMAND
         
         //Exit case
         //Automatically returns false and exits on the other end
-        if(it* == "exit") {
+        if(input ==  "exit") {
+            cout << "returned false";
             return false;
         }
         
+        placeholder.at(0) = *it;
+        compare = placeholder.at(0);
+        
+        
+        
         //Or operator
-        else if(*it == "||") {
-            temp = new Or();
+        if(compare == "||") {
             it++;
-            command = it*;
+            placeholder.at(0) = *it;
+            command = placeholder.at(0);
+            cout << "Inside or" << command << endl;
         }
         
         //And operator
-        else if(*it == "&&") {
-            temp = new And();
+        else if(compare == "&&") {
             it++;
-            command = it*;
+            placeholder.at(0) = *it;
+            command = placeholder.at(0);
+             cout << "Inside and" << command << endl;
         }
         
         //Semi Operator
+        else if (compare == ";") {
+            it++;
+            placeholder.at(0) = *it;
+            command = placeholder.at(0);
+            cout << "Inside and" << command << endl;
+             
+        }
         else {
-            temp = new Semi();
-            command = it*;
+            command = compare;
+            cout << "Inside semi: " << command << endl;
         }
         
+        
         // increment the iterator
-        it++;
+        if (it != toke.end()) {
+            it++;
+            if (it != toke.end()){
+                placeholder.at(0) = *it;
+                compare = placeholder.at(0);
+                cout << "this" << endl;
+            }
+            else {
+                is_command_only = true;
+                cout << "that" << endl;
+            }
+        }
+        
         
         //checks if its a command only
-        if (it* == "||" || it* == "&&" || it* == ";") {
+        if (compare == "||" || compare == "&&" || compare == ";") {
             is_command_only = true;
         }
         
-        
+        //If it isnt the end of the command
         while (is_command_only == false) {
             
-            if (it* == "#") {
-            
-                while(it != toke.end() || it* != "||"  || it* != ";" || it* != "&&") {
+            //Comment checking
+            if (compare ==  "#") {
+                
+                //Loop 
+                while(it != toke.end() || !strcmp((*it).c_str(), "||")  || !strcmp((*it).c_str(),  ";") || !strcmp((*it).c_str() ,  "&&")) {
                     it++;
                     is_command_only = true;
+                    
                 } 
                
             }
-            
             else {
-                
-                args.push_back(it*);
+                placeholder.at(0) = *it;
+                tester = placeholder.at(0);
+                cout << "tester is: " << tester << endl;
+                args.push_back(tester);
                 it++;
                 if(it == toke.end()) {
                     is_command_only = true;
                 }
                 else {
-                    if (it* == "||" || it* == "&&" || it* == ";") {
+                    placeholder.at(0) = *it;
+                    compare = placeholder.at(0);
+                    if (compare == "||" || compare == "&&" || compare == ";") {
                         is_command_only = true;
                     }
                 }
-            
             }
-            
-            
         }
         
-        
-        InputStorage input(command, args);
-        executecmd(input);
+       
+        Inputstorage input(command, args);
+        is_command_only = executecmd(input);
         
         
     }
     
     
 }
+
+
+
 
 #endif
