914e19bd448dd07de72d0cfc969d89ecc18e67c1
nbabu001@ucr.edu <nbabu001@ucr.edu>
2016-11-03 03:38:25 +0000

    took out extra .h file

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include "Parse.h"
 #include "executecmd.h"
 #include "Inputstorage.h"
-#include "Userinfo.h"
+//#include "UserInfo.h"
 
 using namespace std;
 int main(int argc, char** argv) {
@@ -35,7 +35,30 @@ int main(int argc, char** argv) {
     while(1) {
          //get_info();
         //print $user@host()
-         get_info();
+         //this array will hold the host info
+    char hostinfo[2048];
+    hostinfo[2047] = '\0';
+    // this char* will hold the info to user 
+    char *userlogin;
+    // get login
+    userlogin = getlogin();
+    // get user
+    gethostname(hostinfo, 2047);
+    int i = 0;
+    //output user info
+    while(userlogin[i] != '\0'){
+        cout << userlogin[i];
+        i++;
+    }
+    cout << "@";
+    int x = 0;
+    //output host info
+    while(hostinfo[x] != '\0'){
+        cout << hostinfo[x];
+        x++;
+    }
+    cout << "$ ";
+         
         string str;
         getline(cin, str);
         if (!parse(str)) {
